1236d4f9698f4d85b05f3d4e0203c0749f646951
muris2016 <rsemuris2016@gmail.com>
2015-10-22 19:36:16 +0200

    Ejercicio 4 hecho

--- client.py
+++ client.py
@@ -5,28 +5,29 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+if __name__ == "__main__":
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+    # Cliente UDP simple.
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+    # Dirección IP del servidor.
+    SERVER = (sys.argv[1])
+    PORT = int(sys.argv[2])
+    MSG = ' '.join(a for a in sys.argv[3:])
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
+    my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((SERVER, PORT))
 
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
+    print("Enviando: " + MSG)
+    my_socket.send(bytes(MSG, 'utf-8'))
+    data = my_socket.recv(1024)
 
-print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
+    print('Recibido -- ', data.decode('utf-8'))
+    print("Terminando socket...")
 
-# Cerramos todo
-my_socket.close()
-print("Fin.")
+    # Cerramos todo
+    my_socket.close()
+    print("Fin.")
