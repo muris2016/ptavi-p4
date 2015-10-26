bfa2333916c977980df2d671b508a530e782aa36
muris2016 <rsemuris2016@gmail.com>
2015-10-26 14:46:46 +0100

    Ejercicio 8

--- server.py
+++ server.py
@@ -6,32 +6,53 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import json
+import time
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
     client_dicc = {}
+
+    def clean_clients(self):
+        clean_list = []
+        for client in self.client_dicc:
+            expiration = time.mktime(time.strptime(self.client_dicc[client]["expires"], '%Y-%m-%d %H:%M:%S'))
+            if expiration < time.time():
+                clean_list.append(client)
+        for client in clean_list:
+                del self.client_dicc[client]
+
+    def register2json(self):
+        with open('json_file.json', 'w') as outfile:
+            json.dump(self.client_dicc, outfile, sort_keys=True, indent=4)
+
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
         IP = self.client_address[0]
         PORT = self.client_address[1]
         print (IP, PORT, "wrote:")
 
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read().decode('utf-8')
-
-            # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
             print(line)
-            
             if 'REGISTER' in line:
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                self.clean_clients()
                 address = line.split()[1].split(':')[1]
-                self.client_dicc[address] = IP
-            # Si no hay más líneas salimos del bucle infinito
+                expires = int(line.split()[-1])
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                if expires != 0:
+                    time_expire = expires + time.time()
+                    self.client_dicc[address] = {'address': IP,
+                                                 'expires': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_expire))}
+                else:
+                    try:
+                        del self.client_dicc[address]
+                    except KeyError:
+                        break
+                self.register2json()
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
