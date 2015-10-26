453ce8a8db41d3a4d88090e8128ce9d6c7ce1df2
muris2016 <rsemuris2016@gmail.com>
2015-10-26 15:32:33 +0100

    Ejercicio 9 hecho

--- server.py
+++ server.py
@@ -22,11 +22,19 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             if expiration < time.time():
                 clean_list.append(client)
         for client in clean_list:
-                del self.client_dicc[client]
+            del self.client_dicc[client]
 
     def register2json(self):
-        with open('json_file.json', 'r') as outfile:
+        with open('json_file.json', 'w') as outfile:
             json.dump(self.client_dicc, outfile, sort_keys=True, indent=4)
+
+    def json2register(self):
+        try:
+            with open('json_file.json', 'r') as outfile:
+                json_str = outfile.read()
+                self.client_dicc = json.loads(json_str)
+        except:
+            pass
 
     def handle(self):
         IP = self.client_address[0]
@@ -40,6 +48,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             print(line)
             if 'REGISTER' in line:
                 self.clean_clients()
+                self.json2register()
                 address = line.split()[1].split(':')[1]
                 expires = int(line.split()[-1])
                 self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
@@ -55,7 +64,6 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 self.register2json()
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
     PORT = int(sys.argv[1])
     serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
