9715066496d1fe0f513b924742859609c4355453
muris2016 <rsemuris2016@gmail.com>
2015-10-30 10:48:12 +0100

    Version mejorada

--- server.py
+++ server.py
@@ -9,59 +9,75 @@ import sys
 import json
 import time
 
+DATE_F = '%Y-%m-%d %H:%M:%S'
+
+
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-    client_dicc = {}
+    client_dict = {}
 
     def clean_clients(self):
-        clean_list = []
-        for client in self.client_dicc:
-            expiration = time.mktime(time.strptime(self.client_dicc[client]["expires"], '%Y-%m-%d %H:%M:%S'))
-            if expiration < time.time():
-                clean_list.append(client)
+        """
+        Clean's dictionary users with expired term
+        """
+        clean_list = [client for client in self.client_dict
+                      if time.mktime(time.strptime(self.client_dict[client]
+                                     ["expires"], DATE_F)) < time.time()]
         for client in clean_list:
-            del self.client_dicc[client]
+            del self.client_dict[client]
 
     def register2json(self):
+        """
+        Print on a json file the client dict's information
+        """
         with open('json_file.json', 'w') as outfile:
-            json.dump(self.client_dicc, outfile, sort_keys=True, indent=4)
+            json.dump(self.client_dict, outfile, sort_keys=True, indent=4)
 
     def json2register(self):
+        """
+        Get of json file information about clients, who made REGISTER method
+        """
         try:
             with open('json_file.json', 'r') as outfile:
                 json_str = outfile.read()
-                self.client_dicc = json.loads(json_str)
+                self.client_dict = json.loads(json_str)
         except:
             pass
 
     def handle(self):
+        """
+        Recieve and arrange the request of REGISTER method
+        """
+        self.json2register()
+        self.clean_clients()
         IP = self.client_address[0]
         PORT = self.client_address[1]
         print (IP, PORT, "wrote:")
 
         while 1:
-            line = self.rfile.read().decode('utf-8')
+            line = self.rfile.read().decode('utf-8').split()
             if not line:
                 break
-            print(line)
-            if 'REGISTER' in line:
-                self.clean_clients()
-                self.json2register()
-                address = line.split()[1].split(':')[1]
-                expires = int(line.split()[-1])
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                if expires != 0:
-                    time_expire = expires + time.time()
-                    self.client_dicc[address] = {'address': IP,
-                                                 'expires': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time_expire))}
+            print(" ".join(line))
+            try:
+                if 'REGISTER' in line:
+                    address = line[1].split(':')[1]
+                    expires = int(line[-1])
+                    if expires != 0:
+                        exp_t = expires + time.time()
+                        exp_t = time.strftime(DATE_F, time.localtime(exp_t))
+                        self.client_dict[address] = {'address': IP,
+                                                     'expires': exp_t}
+                    else:
+                        del self.client_dict[address]
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                 else:
-                    try:
-                        del self.client_dicc[address]
-                    except KeyError:
-                        break
-                self.register2json()
+                    self.wfile.write(b"SIP/2.0 400 BAD REQUEST\r\n\r\n")
+            except:
+                self.wfile.write(b"SIP/2.0 400 BAD REQUEST\r\n\r\n")
+            self.register2json()
 
 if __name__ == "__main__":
     PORT = int(sys.argv[1])
