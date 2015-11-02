17c9e03a3f5154af938f3dab46403e57ccf7a0a5
muris2016 <rsemuris2016@gmail.com>
2015-11-02 10:45:02 +0100

    Version final

--- client.py
+++ client.py
@@ -8,29 +8,25 @@ import socket
 import sys
 
 if __name__ == "__main__":
-
-    # Cliente UDP simple.
-
-    # Dirección IP del servidor.
     SERVER = (sys.argv[1])
     PORT = int(sys.argv[2])
     if sys.argv[3] == 'register':
-        MSG = 'REGISTER sip:%s SIP/2.0\r\n\r\n' %(sys.argv[4])
+        MSG = 'REGISTER sip:%s SIP/2.0\r\nExpires: %s\r\n\r\n' % (sys.argv[4],
+                                                                  sys.argv[5])
     else:
         MSG = ' '.join(a for a in sys.argv[3:])
 
-    # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
     my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((SERVER, PORT))
 
-    print("Enviando: " + MSG)
+    print(MSG)
     my_socket.send(bytes(MSG, 'utf-8'))
+
     data = my_socket.recv(1024)
 
-    print('Recibido -- ', data.decode('utf-8'))
-    print("Terminando socket...")
+    print(data.decode('utf-8'))
+    print("Finishing socket...")
 
-    # Cerramos todo
     my_socket.close()
-    print("Fin.")
+    print("End.")
